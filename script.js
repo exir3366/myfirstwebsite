33e355994b8bf99284c878094acf81a45e32c082
Ruderick Veve <116129374+exir3366@users.noreply.github.com>
2022-11-23 23:19:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sayHi() {
     let userName = prompt("What is your name?");
     if (userName == "") ; 
-        while (userName == "" || userName == null) {
+        while (userName == null) {
         alert(Why didn't I receive any input?)
-            userName = prompt("TELL ME YOUR NAME OR I WILL EXPLODE!!!)
+        userName = prompt("TELL ME YOUR NAME OR I WILL EXPLODE!!!)
    
