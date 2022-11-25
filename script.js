5fc966650e8daeca9bd9cb7f51f47ab28bb06e26
Ruderick Veve <rveve2012@gmail.com>
2022-11-24 20:19:51 -0800

    git a

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function welcomePrompt()  {
+function welcomePrompt()  { 
     let response  = prompt('Do you like the outdoors? Yes?No?')
     if (response !== 'yes') {
     console.log(response)
-    response = prompt ("That is great because I would have hated to ask again!! Are you interested in hiriing us? Yes?")
+    continue;
+} else
+    break;
+}
+}
+
+function yesOrNo() {
+let opinion = prompt('yes or no?');
+console.log(opinion);
+opinion = opinion.toLowerCase();
+console.log(opinion);
+
+if (opinion === 'yes') {
+alert('Yay!');
+} else if (opinion === 'no') {
+alert('Sadage');
+} else {
+alert('What?');
+}
+return;
+}
+
+function changeMind() {
+let secondOpinion = confirm('do you want to change your mind?');
+console.log(secondOpinion);
+if (secondOpinion == true) {
+alert('You Changed Your Mind');
+} else {
+alert('You didn\'t Change Your Mind');
+}
+return;
+}
