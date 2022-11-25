e85ea2db07fbe2dcb524651237a5f4ebb2b0cd0f
Ruderick Veve <116129374+exir3366@users.noreply.github.com>
2022-11-24 23:09:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function msg(){  
- prompt("Hello, Welcome To V Squared Home Services" "Would you like to play a game?");  
+ alert("Hello, Welcome To V Squared Home Services" "Would you like to play a game?");  
 }  
