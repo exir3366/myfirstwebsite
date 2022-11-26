e16cc50f3aa119fa96a0924595b44407ae1e87e1
Ruderick Veve <116129374+exir3366@users.noreply.github.com>
2022-11-25 23:50:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function msg(){
  alert("Hello, Welcome To V Squared Home Services");  
 }
 function welcomeMsg (usersName) {
-prompt('What is your name?);
+alert('What is your name?);
 console.log ('The users name is:' + usersName');
  document.write ('Hello, and welcome to my page, + usersName');
 }
